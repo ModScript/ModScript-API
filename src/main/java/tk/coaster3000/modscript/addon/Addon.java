c797d64595ef7b23d8c3fc70441e82b6dd39c15f
Christopher Krier <ckrier.3000@gmail.com>
2015-08-19 16:22:51 -0400

    Initial Base API
    - Initial Addon interface specifications.
    - Initial Addon Loading API

---
+++ src/main/java/tk/coaster3000/modscript/addon/Addon.java
@@ -0,0 +1,57 @@
+package tk.coaster3000.modscript.addon;
+
+import tk.coaster3000.modscript.ModScriptAPI;
+
+public interface Addon {
+	/**
+	 * Retrieves the name of the addon.
+	 * @return String
+	 */
+	String getName();
+
+	/**
+	 * Retrieves the addon's description.
+	 * @return String
+	 */
+	String getDescription();
+
+	/**
+	 * Retrieves the addon's version.
+	 * @return String
+	 */
+	String getVersion();
+
+	/**
+	 * Retrieves the supported plugin's version. <br/>
+	 * It is used to make sure to inform server owner's that the addon is, or isn't supported by the current version od ModScript.
+	 * @return String
+	 */
+	String getSupportedVersion();
+
+	/**
+	 * Retrieves an array of authors of this addon.
+	 * @return String Array
+	 */
+	String[] getAuthors();
+
+	/**
+	 * Returns whether or not the addon is enabled.
+	 * @return boolean
+	 */
+	boolean isEnabled();
+
+	/**
+	 * Called on initialization of the class.
+	 */
+	void init(ModScriptAPI modScriptAPI);
+
+	/**
+	 * Called when the addon is enabled.
+	 */
+	void enable();
+
+	/**
+	 * Called when the addon is disabled.
+	 */
+	void disable();
+}
---
+++ src/main/scala/tk/coaster3000/modscript/ModScript.scala
@@ -0,0 +1,48 @@
+package tk.coaster3000.modscript
+
+import tk.coaster3000.modscript.addon.Addon
+import tk.coaster3000.modscript.util.{WrappedAddonException, AddonAlreadyExistsException}
+
+abstract class ModScript {
+	abstract def reportAddon(wrappedAddonException: WrappedAddonException)
+
+	private val addons = collection.mutable.Map[String, Addon]()
+
+	protected def getAddons() = addons
+
+	def getAddon(addonName: String) = addons.get(addonName).orNull
+
+	@throws[AddonAlreadyExistsException]
+	def addAddon(addon: Addon) = {
+		if (addons contains addon.getName) throw new AddonAlreadyExistsException(addon)
+		try {
+			addon.enable()
+			addons put (addon.getName, addon)
+		} catch {
+			case e: Throwable => reportAddon(new WrappedAddonException(addon, e))
+		}
+
+	}
+
+	def removeAddon(_addon: Addon) = {
+		val mappedAddon = addons remove _addon.getName
+		if (mappedAddon.nonEmpty) {
+			val addon = mappedAddon.get
+			try {
+				addon.disable()
+			} catch {
+				case e: Throwable => reportAddon(new WrappedAddonException(addon, e));
+			}
+		}
+	}
+}
+
+object ModScript {
+	private var instance: ModScript = null
+
+	protected def setInstance(ms: ModScript) = {
+		instance = ms
+	}
+
+	def getInstance() = instance
+}
